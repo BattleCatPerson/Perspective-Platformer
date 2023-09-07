95e17fd85e84dbae2d960fae54edd7db904ae45f
BattleCatPerson <93836439+BattleCatPerson@users.noreply.github.com>
2023-09-07 11:30:31 -0400

    aw4serdtrguyiu

--- 2D 3D Perspective Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D 3D Perspective Platformer/Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Rigidbody rb;
     [SerializeField] Vector2 input;
     [SerializeField] Vector2 rawInput;
+    [SerializeField] Vector2 filteredInput;
 
     [Header("Horizontal Movement")]
     [SerializeField] bool moving;
@@ -44,10 +45,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] Dimension dimension = Dimension.Two;
     [SerializeField] bool doubleJumped;
     [SerializeField] float doubleJumpForce;
+    [SerializeField] Transform camTransform;
+    [SerializeField] Transform movementDirection;
 
     [Header("Controller Adjustments")]
-    [SerializeField] float lower;
-    [SerializeField] float upper;
+    [SerializeField, Range(0, 1)] float yThreshold;
+    [SerializeField, Range(0, 1)] float xThreshold;
     void Start()
     {
         //dimension = Dimension.Two;
@@ -67,7 +70,9 @@ public class PlayerMovement : MonoBehaviour
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
-        input = SortValue(value.Get<Vector2>());
+        filteredInput = SortValue(rawInput);
+        if (dimension == Dimension.Two) input = (new Vector2(filteredInput.x, 0)).normalized;
+        else input = filteredInput;
         moving = (dimension == Dimension.Two && input.x != 0) || (dimension == Dimension.Three && (input.magnitude > 0));
     }
 
@@ -88,7 +93,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnDash()
     {
-        dashDirection = dimension == Dimension.Two ? new Vector3(input.x, input.y).normalized : new Vector3(input.x, 0, input.y);
+        dashDirection = dimension == Dimension.Two ? new Vector3(filteredInput.x, filteredInput.y).normalized : new Vector3(filteredInput.x, 0, filteredInput.y);
         if (canDash && dashDirection != Vector3.zero) dashInput = true;
     }
     private void OnCollisionEnter(Collision collision)
@@ -178,7 +183,8 @@ public class PlayerMovement : MonoBehaviour
     public void Dash()
     {
         control = 0;
-        rb.velocity = dashDirection * dashForce;
+        if (dimension == Dimension.Two) rb.velocity = dashDirection * dashForce;
+        else rb.velocity = (movementDirection.forward * dashDirection.y + movementDirection.right * dashDirection.x) * dashForce; //fix this
         StartCoroutine("DashDelay");
         canDash = false;
         dashInput = false;
@@ -201,12 +207,12 @@ public class PlayerMovement : MonoBehaviour
     public void Movement3D()
     {
         DashControl();
-
+        movementDirection.eulerAngles = new(0, camTransform.eulerAngles.y);
         if (moving)
         {
             if (((wall != null && Mathf.Abs(transform.position.x + input.x - wall.position.x) > Mathf.Abs(transform.position.x - wall.position.x)) || wall == null) && control >= controlThreshold)
             {
-                rb.AddForce((transform.right * input.x + transform.forward * input.y).normalized * speed * control);
+                rb.AddForce((movementDirection.right * input.x + movementDirection.forward * input.y).normalized * speed * control);
                 if (!dashing)
                 {
                     if (input.x != 0 && input.y != 0)
@@ -288,12 +294,12 @@ public class PlayerMovement : MonoBehaviour
         {
             if (v.y > 0)
             {
-                if (v.y < lower) return Vector2.right * multX;
+                if (v.y < yThreshold) return Vector2.right * multX;
                 else return (new Vector2(multX, 1)).normalized;
             }
             else
             {
-                if (v.y > -lower) return Vector2.right * multX;
+                if (v.y > -yThreshold) return Vector2.right * multX;
                 else return (new Vector2(multX, -1)).normalized;
             }
         }
@@ -301,12 +307,12 @@ public class PlayerMovement : MonoBehaviour
         {
             if (v.x > 0)
             {
-                if (v.x < lower) return Vector2.up * multY;
+                if (v.x < xThreshold) return Vector2.up * multY;
                 else return (new Vector2(1, multY)).normalized;
             }
             else
             {
-                if (v.y > -lower) return Vector2.up * multY;
+                if (v.x > -xThreshold) return Vector2.up * multY;
                 else return (new Vector2(-1, multY)).normalized;
             }
         }
