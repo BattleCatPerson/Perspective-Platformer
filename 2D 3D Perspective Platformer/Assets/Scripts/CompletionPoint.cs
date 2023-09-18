46fcf4d42522063c36f1827d551a36f30345fab2
BattleCatPerson <dankmarkers123@gmail.com>
2023-09-17 23:04:18 -0400

    stuff

---
+++ 2D 3D Perspective Platformer/Assets/Scripts/CompletionPoint.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompletionPoint : MonoBehaviour
+{
+    [SerializeField] Collider pointCollider;
+    [SerializeField] MeshRenderer pointMesh;
+    void Start()
+    {
+        GameManager.onShift += EnablePoint;
+        pointCollider.enabled = false;
+        pointMesh.enabled = false;
+    }
+    public void EnablePoint()
+    {
+        pointCollider.enabled = true;
+        pointMesh.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("YOU WIN");
+    }
+}
--- 2D 3D Perspective Platformer/Assets/Scripts/GameManager.cs
+++ 2D 3D Perspective Platformer/Assets/Scripts/GameManager.cs
@@ -6,12 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager currentGameManager;
     public bool victory;
+    public bool shifted;
 
     public static Action onShift;
     public static Action onVictory;
 
     private void Awake()
     {
+        onShift = null;
+        onVictory = null;
         currentGameManager = this;
     }
 }
